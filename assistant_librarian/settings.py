1c4427713cac14ffb479b416651d69256188f84c
Artem Striver <138004004+ArtemStriver@users.noreply.github.com>
2024-03-01 11:06:08 +0500

    Update settings.py

--- assistant_librarian/settings.py
+++ assistant_librarian/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3%_vhc#5%!88550+8ij%whu^g%^ezar!m3fg+9&udm35@&pv^k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
